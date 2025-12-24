518c153a3feeb9389198f7a32e273ec0f56227b6
anjakob <anjakob@student.42heilbronn.de>
2025-12-24 17:11:28 +0100

    added a and b struct, && renamed func atol() && added ft_strcmp()

--- push_swap.h
+++ push_swap.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: anjakob <anjakob@student.42heilbronn.de>   #+#  +:+       +#+        */
+/*   By: anjakob <anjakob@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025-12-18 13:37:49 by anjakob           #+#    #+#             */
-/*   Updated: 2025-12-18 13:37:49 by anjakob          ###   ########.fr       */
+/*   Created: 2025/12/18 13:37:49 by anjakob           #+#    #+#             */
+/*   Updated: 2025/12/24 16:27:46 by anjakob          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,19 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-long	ft_atoi(const char *nptr);
+typedef struct a_stack
+{
+	size_t	*stack;
+	size_t	len;
+}	a_stack;
+
+typedef struct b_stack
+{
+	size_t	*stack;
+	size_t	len;
+}	b_stack;
+
+long	ft_atol(const char *s);
+int		ft_strcmp(const char *s1, const char *s2);
 
 #endif
