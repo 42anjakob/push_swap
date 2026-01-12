7c6055c1bebb6f80dd337ec186387c5b6e58695c
anjakob <anjakob@student.42heilbronn.de>
2026-01-12 07:16:04 +0100

    Fixed issues hanlding **argv input "1 2 3" && 1 2 3 the same way && finished radix_sort()

--- push_swap.c
+++ push_swap.c
@@ -6,12 +6,11 @@
 /*   By: anjakob <anjakob@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/18 13:37:47 by anjakob           #+#    #+#             */
-/*   Updated: 2025/12/25 18:26:17 by anjakob          ###   ########.fr       */
+/*   Updated: 2026/01/12 07:12:43 by anjakob          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-// #include <stdio.h>
 
 // SORT
 void	simple_sort(t_a_stack *a, t_b_stack *b)
@@ -26,50 +25,93 @@ void	simple_sort(t_a_stack *a, t_b_stack *b)
 
 void	radix_sort(t_a_stack *a, t_b_stack *b)
 {
-	t_a_stack	*a_rray;
-	t_b_stack	*b_rray;
+	size_t	i;
 
-	a_rray = a;
-	b_rray = b;
-	a_rray->len = b_rray->len;
+	i = 0;
+	while (i < a->len)
+	{
+		if ((a->stack[0] & 1) == 0)
+		{
+			p(a->stack, b->stack, &a->len, &b->len);
+			write(1, "pb\n", 3);
+		}
+		else if ((a->stack[0] & 1) == 1)
+		{
+			r(a->stack, &a->len);
+			write(1, "ra\n", 3);
+			i++;
+		}
+	}
+	while (b->len > 0)
+	{
+		p(b->stack, a->stack, &b->len, &a->len);
+		write(1, "pa\n", 3);
+	}
+}
+
+void	bits_loop(t_a_stack *a, t_b_stack *b)
+{
+	size_t	bits;
+	size_t	i;
+
+	bits = 32;
+	while (bits--)
+	{
+		radix_sort(a, b);
+		i = 0;
+		while (i < a->len)
+		{
+			a->stack[i] = a->stack[i] >> 1;
+			i++;
+		}
+	}
 }
 
 int	main(const int argc, char **argv)
 {
 	t_a_stack	a;
 	t_b_stack	b;
-	char		**arg;
+	char		**arg_s;
+	int			arg_c;
 
-	arg = argv;
+	arg_s = &argv[1];
+	arg_c = argc - 1;
+
 	// error handling
-	if (argc < 2)
+	if (arg_c == 0)
 		return (0);
-	else if (argc == 2)
+	else if (arg_c == 1)
 	{
-		arg = ft_split(argv[1], ' ');
-		if (!arg)
-			return (err(argc, arg, a.stack, b.stack), 1);
+		arg_s = ft_split(argv[1], ' ');
+		if (!arg_s)
+			return (err(arg_c, arg_s, a.stack, b.stack), 1);
+		arg_c = 0;
+		while (arg_s[arg_c])
+			arg_c++;
 	}
-	if (!is_int(arg) || is_dup(arg))
-		return (err(argc, arg, a.stack, b.stack), 2);
+	if (!is_int(arg_s) || is_dup(arg_s))
+		return (err(arg_c, arg_s, a.stack, b.stack), 2);
+
 	// alloc stacks
-	a.stack = malloc(sizeof(size_t) * argc);
+	a.stack = malloc(sizeof(size_t) * arg_c);
 	if (!a.stack)
-		return (err(argc, arg, a.stack, b.stack), 3);
-	b.stack = malloc(sizeof(size_t) * argc);
+		return (err(arg_c, arg_s, a.stack, b.stack), 3);
+	b.stack = malloc(sizeof(size_t) * arg_c);
 	if (!b.stack)
-		return (free(a.stack), err(argc, arg, a.stack, b.stack), 4);
-	a.len = argc - 1;
+		return (err(arg_c, arg_s, a.stack, b.stack), 4);
+	a.len = arg_c;
 	b.len = 0;
+
 	// sort
-	fill(a.stack, arg);
-	if (argc <= 6)
+	fill(a.stack, arg_s);
+	if (a.len <= 6)
 		simple_sort(&a, &b);
 	else
-		radix_sort(&a, &b);
+		bits_loop(&a, &b);
+		
 	// free
-	if (argc == 2)
-		ft_freeptr((void **)arg);
+	if (arg_c == 2)
+		ft_freeptr((void **)arg_s);
 	free(a.stack);
 	free(b.stack);
 	return (0);
--- utils.c
+++ utils.c
@@ -6,7 +6,7 @@
 /*   By: anjakob <anjakob@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/18 14:10:13 by anjakob           #+#    #+#             */
-/*   Updated: 2025/12/25 18:59:14 by anjakob          ###   ########.fr       */
+/*   Updated: 2026/01/12 07:12:46 by anjakob          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,23 +29,22 @@ int	is_int(char **arg)
 	int		i;
 	int		j;
 
-	i = 1;
-	while (arg[i++])
+	i = 0;
+	while (arg[i])
 	{
 		j = 0;
-		if (arg[i][j] == '-' || arg[i][j] == '+')
+		if (arg[i][j++] == '-' || arg[i][j++] == '+')
 		{
 			if (arg[i][1] == '\0')
 				return (0);
-			j++;
 		}
 		while (arg[i][j++])
 		{
-			if (arg[i][j] < '0' || arg[i][j] > '9')
+			if (arg[i][j - 1] < '0' || arg[i][j - 1] > '9')
 				return (0);
 		}
 		num = ft_atol(arg[i]);
-		if (arg[i][0] == '\0' || j > 11
+		if (arg[i++][0] == '\0' || j > 12
 			|| (num < -2147483648 || num > 2147483647))
 			return (0);
 	}
@@ -57,7 +56,7 @@ int	is_dup(char **arg)
 	int	i;
 	int	j;
 
-	i = 1;
+	i = 0;
 	while (arg[i])
 	{
 		j = 1;
@@ -77,13 +76,12 @@ void	fill(size_t *a, char **arg)
 	long	n;
 	int		i;
 
-	i = 1;
+	i = 0;
 	while (arg[i])
 	{
 		n = ft_atol(arg[i]);
 		n += 2147483648;
-		a[i - 1] = n;
+		a[i] = n;
 		i++;
 	}
-	a[i - 1] = 0;
 }
