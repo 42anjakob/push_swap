aaa91401dcc79adab3fd3334e9fd9e7a9b57eeae
anjakob <anjakob@student.42heilbronn.de>
2026-01-18 19:08:01 +0100

    improved logic of 1 if case in if_three()

--- less_then_six.c
+++ less_then_six.c
@@ -6,7 +6,7 @@
 /*   By: anjakob <anjakob@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/12 23:55:55 by anjakob           #+#    #+#             */
-/*   Updated: 2026/01/16 20:13:25 by anjakob          ###   ########.fr       */
+/*   Updated: 2026/01/18 18:22:46 by anjakob          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,14 +30,15 @@ void	if_three(t_a_stack *a, size_t ft_pos)
 		write(1, "rra\n", 4);
 	else if (a->stack[0] == 0 + ft_pos && a->stack[1] == 2 + ft_pos
 		&& a->stack[2] == 1 + ft_pos
-		&& r(a->stack, a->len) && s(a->stack) && rr(a->stack, a->len))
-		write(1, "ra\nsa\nrra\n", 10);
+		&& s(a->stack) && r(a->stack, a->len))
+		write(1, "sa\nra\n", 6);
 }
 
 void	if_four(t_a_stack *a, t_b_stack *b)
 {
-	if (a->stack[0] == 0 && p(a->stack, b->stack, &a->len, &b->len))
+	if (a->stack[0] == 0)
 	{
+		p(a->stack, b->stack, &a->len, &b->len);
 		write(1, "pb\n", 3);
 		if_three(a, 1);
 		p(b->stack, a->stack, &b->len, &a->len);
